3019498875203b3f075212048efdfd0d03fea0cc
maxchtse <tsechunghei97@outlook.com>
2024-01-23 18:13:30 +0800

    Add a function to schedule a delay for splash screen

--- app/src/main/java/com/company/noteapp/SplashActivity.java
+++ app/src/main/java/com/company/noteapp/SplashActivity.java
@@ -2,7 +2,9 @@ package com.company.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        /* To schedule a delay of 1000ms for the splash screen before transitioning to the main activity */
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }, 1000);
     }
 }
